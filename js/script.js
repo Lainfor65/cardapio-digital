50d32e6bb86d1bbd77f33b293d4894bc483868c6
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-06-30 18:19:54 +0100

    update and add

---
+++ js/script.js
@@ -0,0 +1,5 @@
+new SimpleSlide({
+    slide: "welcome", // nome do atributo data-slide="principal"
+    time: 3000, // tempo de transição dos slides
+    pauseOnHover: false // pausa a transição automática
+  });
