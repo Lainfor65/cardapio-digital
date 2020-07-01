a535e3ef76e33e769eb0902224402d906a618024
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-07-01 16:21:51 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-new SimpleSlide({
+if (window.SimpleSlide) {
+  new SimpleSlide({
     slide: "welcome", // nome do atributo data-slide="principal"
     time: 3000, // tempo de transição dos slides
     pauseOnHover: false // pausa a transição automática
   });
+}
+
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
