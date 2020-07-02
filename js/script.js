41f310851d5b3c77dd3e8d9280ff89043d0bb7d9
Wanderson Oliveira Silva <wanderson1873@live.com>
2020-07-03 00:11:17 +0100

    update

--- js/script.js
+++ js/script.js
@@ -5,16 +5,3 @@ if (window.SimpleSlide) {
     pauseOnHover: false // pausa a transição automática
   });
 }
-
-window.onscroll = function () { myFunction() };
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
