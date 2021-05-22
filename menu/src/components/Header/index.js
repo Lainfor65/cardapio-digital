e3c4492c13e169538259fecee08e9b1f979373dd
Wanderson Oliveira <wanderson1873@live.com>
2021-05-22 15:36:45 +0100

    add Header

--- menu/src/components/Header/index.js
+++ menu/src/components/Header/index.js
@@ -1,24 +1,27 @@
-import styles from './styles.module.css'
+import styles from './styles.module.scss';
 
-const index = () => {
+export function Header() {
    return (
       <>
          <div className={styles.bg}>
-               <img src="logo.png" alt="Logo Carnes do COnvento" />
-               <p>Bem-vindo ao Carnes do Convento</p>
-               <h1>Menu</h1>
-            </div>
+            <img src="logo.png" alt="Logo Carnes do Convento" />
+            <p>Bem-vindo ao Carnes do Convento</p>
+            <h1>Ementa</h1>
+         </div>
 
          <header className={styles.header}>
-            <button aria-label="Menu"></button>
+            <button className={`${styles.btnMenu} ${styles.btnMenuActive}`} aria-label="Menu" />
             <div className="search">
-               <form><input type="text" />Pesquisa</form>
-               <img src="icon-search" alt="Icone de pesquisa" />
+               <form>
+                  <input type="text" placeholder="Ex: Fraldinha..." name="search" id="search" />
+                  <label htmlFor="search">
+                     <img src="search.svg" alt="Icone de pesquisa" />
+                  </label>
+               </form>
             </div>
             <button id="language">PT</button>
          </header>
       </>
-   )
+   );
 }
 
-export default index
--- menu/src/pages/_app.js
+++ menu/src/pages/_app.js
@@ -1,6 +1,19 @@
+import React from 'react';
+
+import '../style/global.scss';
+import { Header } from '../components/Header';
+
+import styles from '../style/app.module.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+        <Header />
+        <main>
+          <Component {...pageProps} />
+        </main>
+    </div>
+  )
 }
 
 export default MyApp
